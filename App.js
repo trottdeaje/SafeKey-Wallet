5fd47862ad582b6b750d929b3bd7b6ac7d49ae99
DeajeT <deajetrott@gmail.com>
2021-08-09 22:43:30 -0300

    Added PWA Screenshots & Icons
    # Added screenshots and shortcut icons to manifest.
    
    # Removed lazy loading from select document screen and QR scan screen

--- App.js
+++ App.js
@@ -18,10 +18,8 @@ import {
 import * as Analytics from "expo-firebase-analytics";
 // Importing the screens
 import HomeScreen from "./screens/HomeScreen";
-const SelectDocumentScreen = loadable(() =>
-  import("./screens/SelectDocumentScreen")
-);
-const QrScanScreen = loadable(() => import("./screens/QrScanScreen"));
+import SelectDocumentScreen from "./screens/SelectDocumentScreen";
+import QrScanScreen from "./screens/QrScanScreen";
 const QrListScreen = loadable(() => import("./screens/QrListScreen"));
 const PassInfoScreen = loadable(() => import("./screens/NoticeSafeKeyScreen"));
 const VaxInfoScreen = loadable(() =>
